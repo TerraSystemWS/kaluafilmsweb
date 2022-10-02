7ab51f07c09e52a5c0e3c065813f5416797f9b04
Ailton Duarte <adidas.coyas@gmail.com>
2022-10-02 09:24:37 -0100

    creta cliente page for teste

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,15 +2,48 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
+import sanityClient from "../sanityClient";
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ posts }) => {
+	// console.log(posts);
 	return (
-		<div>
-			<h1 className="text-3xl font-bold">Hello world!</h1>
-			<h3>I&apos;m TerraSystem</h3>
-			<h2>LTD SIDE</h2>
-		</div>
+		<main>
+			{/* <pre>{JSON.stringify(posts, null, 2)}</pre> */}
+			<h1>================= Posts =====================</h1>
+			{posts.map((post) => (
+				<>
+					<article key={post._id}>
+						<h2>
+							<Link href={`/blog/${post.slug}`}>
+								<a>{post.title}</a>
+							</Link>
+						</h2>
+					</article>
+					<hr></hr>
+				</>
+			))}
+		</main>
 	);
 };
+
+export async function getStaticProps() {
+	const posts = await sanityClient.fetch(`
+	*[_type == 'posts']
+	{
+		_id,
+		'slug': slug.current,
+		title,
+		cover,
+		resumo
+	}
+	`);
+
+	return {
+		props: {
+			posts,
+		},
+	};
+}
 
 export default Home;
