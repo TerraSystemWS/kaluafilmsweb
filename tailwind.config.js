fd02685888bdebb0090383ba9bf53e8f764c0820
Ailton Duarte <adidas.coyas@gmail.com>
2022-10-10 13:34:49 -0100

    create tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		screens: {
+			sm: "480px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+		extend: {
+			colors: {
+				laranja: "#E74C3C",
+				"laranja-claro": "#EB6F62",
+				"laranja-escuro": "#e8563b",
+				branco: "#FFFFFF",
+				preto: "#000000",
+				"cinza-claro": "#8A8A8A",
+				"cinza-escuro": "#8895B3",
+			},
+		},
 	},
 	plugins: [],
 };
