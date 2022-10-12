8f7f2628cfd759dffa6d153fa4c934b453b64381
Ailton Duarte <adidas.coyas@gmail.com>
2022-10-12 10:42:36 -0100

    footer & navbar

---
+++ components/Footer/index.tsx
@@ -0,0 +1,107 @@
+import Image from "next/image";
+
+const Footer = () => {
+	return (
+		<footer className="bg-laranja border-t">
+			{/* <!-- Flex Container --> */}
+			<div className="container flex flex-col-reverse justify-between px-6 py-10 mx-auto space-y-8 md:flex-row md:space-y-0">
+				{/* <!-- Logo and social links container --> */}
+				<div className="flex flex-col-reverse items-center justify-between space-y-12 md:flex-col md:space-y-0 md:items-start">
+					<div className="mx-auto my-6 text-center text-white md:hidden">
+						Copyright &copy; 2022, Todos os direitos reservados
+					</div>
+					{/* <!-- Logo --> */}
+					<div>
+						<Image
+							src="/k_logo.png"
+							className="h-8"
+							alt="logo da kalua films"
+							width={300}
+							height={100}
+						/>
+					</div>
+					{/* <!-- Social Links Container --> */}
+					<div className="flex justify-center space-x-4">
+						{/* <!-- Link 1 -->
+						<a href="#">
+							<img src="img/icon-facebook.svg" alt="" className="h-8" />							
+						</a>
+						{/* <!-- Link 2 --> * /}
+						<a href="#">
+							<img src="img/icon-youtube.svg" alt="" className="h-8" />
+						</a>
+						{/* <!-- Link 3 --> * /}
+						<a href="#">
+							<img src="img/icon-twitter.svg" alt="" className="h-8" />
+						</a>
+						{/* <!-- Link 4 --> * /}
+						<a href="#">
+							<img src="img/icon-pinterest.svg" alt="" className="h-8" />
+						</a>
+						{/* <!-- Link 5 --> * /}
+						<a href="#">
+							<img src="img/icon-instagram.svg" alt="" className="h-8" />
+						</a> */}
+					</div>
+				</div>
+				{/* <!-- List Container --> */}
+				<div className="flex justify-around space-x-32">
+					<div className="flex flex-col space-y-3 text-white">
+						<a href="#" className="hover:text-brightRed">
+							Home
+						</a>
+						<a href="#" className="hover:text-brightRed">
+							Portfolio
+						</a>
+						<a href="#" className="hover:text-brightRed">
+							Blog
+						</a>
+						<a href="#" className="hover:text-brightRed">
+							Contato
+						</a>
+					</div>
+					<div className="flex flex-col space-y-3 text-white">
+						<a href="#" className="hover:text-brightRed">
+							Carreira
+						</a>
+						<a href="#" className="hover:text-brightRed">
+							FAQ
+						</a>
+						<a href="#" className="hover:text-brightRed">
+							Privacy Policy
+						</a>
+						<a
+							href="https://kriolstock.com"
+							target="_blank"
+							rel="noreferrer"
+							className="hover:text-brightRed"
+						>
+							KriolStock
+						</a>
+					</div>
+				</div>
+
+				{/* <!-- Input Container --> */}
+				<div className="flex flex-col justify-between">
+					<form>
+						<div className="flex space-x-3">
+							<input
+								type="text"
+								className="flex-1 px-4 rounded-full focus:outline-none border-2"
+								placeholder="pesquisas..."
+							/>
+							<button className="px-6 py-2 text-laranja rounded-full bg-branco hover:bg-laranja  hover:text-branco hover:border hover:border-branco focus:outline-none">
+								pesquiar
+							</button>
+						</div>
+					</form>
+					<div className="hidden text-white md:block">
+						Copyright &copy; 2022, All Rights Reserved
+					</div>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
---
+++ components/Navbar/index.tsx
@@ -0,0 +1,90 @@
+import Image from "next/image";
+import React, { useState } from "react";
+
+const Navbar = () => {
+	const [isOpen, setIsOpen] = useState(false);
+	// const [class1, setClass1] = useState(" ");
+	// const [class2, setClass2] = useState(" ");
+
+	// let abri = "";
+	// let nav = "";
+
+	// const open_menu = () => {
+	// 	setIsOpen(!isOpen);
+	// 	if (isOpen) {
+	// 		setClass1(" open");
+	// 		setClass2(" flex hidden");
+	// 	}
+
+	// 	// setOpen(false);
+	// };
+
+	return (
+		// Navbar
+		<nav className="relative container bg-branco mx-auto p-6 border-b border-b-laranja">
+			{/* flex container */}
+			<div className="flex items-center justify-between">
+				{/* logo */}
+				<div className="pt-2">
+					<Image
+						src="/k_logo_b.png"
+						alt="logo da kalua films"
+						width={200}
+						height={50}
+					/>
+				</div>
+				{/* menu items */}
+				<div className="hidden md:flex space-x-12">
+					<a href="" className="text-laranja hover:text-laranja-claro">
+						Home
+					</a>
+					<a href="" className="text-laranja hover:text-laranja-claro">
+						Portfolio
+					</a>
+					<a href="" className="text-laranja hover:text-laranja-claro">
+						Blog
+					</a>
+					<a href="" className="text-laranja hover:text-laranja-claro">
+						Contato
+					</a>
+				</div>
+				{/* humburger item */}
+				<button
+					id="menu-btn"
+					className={`block hamburger md:hidden focus:outline-none ${
+						isOpen ? "open" : ""
+					}`}
+					onClick={() => setIsOpen(!isOpen)}
+				>
+					<span className="hamburger-top"></span>
+					<span className="hamburger-middle"></span>
+					<span className="hamburger-bottom"></span>
+				</button>
+			</div>
+			{/* mobile menu */}
+			<div className="md:hidden">
+				<div
+					id="menu"
+					className={`absolute flex-col items-center self-end  py-8 mt-10 space-y-6 font-bold bg-branco sw:w-auto sw:self-center left-6 right-6 drop-shadow-md ${
+						isOpen ? " flex" : " hidden"
+					}`}
+				>
+					<a href="" className="text-laranja hover:text-laranja-claro">
+						Home
+					</a>
+					<a href="" className="text-laranja hover:text-laranja-claro">
+						Portfolio
+					</a>
+					<a href="" className="text-laranja hover:text-laranja-claro">
+						Blog
+					</a>
+					<a href="" className="text-laranja hover:text-laranja-claro">
+						Contato
+					</a>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
